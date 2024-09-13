54bf0fb54277df47060df2880951dc6b9efa2389
Abigail Acuña <acuaabigail@yahoo.com>
2024-09-12 18:45:34 -0600

    Agregar archivos de proyecto.

---
+++ LibrosAPI/Models/LibrosDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography.X509Certificates;
+
+namespace LibrosAPI.Models
+{
+    public class LibrosDBContext : DbContext
+    {
+        public LibrosDBContext(DbContextOptions<LibrosDBContext> options) : base(options) 
+        {
+           
+        }
+
+        public DbSet<Libro> Libros { get; set; }
+    }
+}
